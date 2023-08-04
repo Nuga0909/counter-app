658fd3792faa301cee5960cd785eaee0ad4c47eb
Adeshina Adenuga <Nuga0909@gmail.com>
2023-08-04 08:45:40 +0100

    Update simple counter app react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [interval, setInterval] = useState(1);
+
+  const handleCounterChange = (event) => {
+    setInterval(Number(event.target.value));
+
+  }
+
+  const incrementCounter = () => {
+    setCounter((prev) => prev + interval);
+    // setCounter(counter+interval);
+  }
+
+  const decrementCounter = () => {
+    if (counter > 0) {
+      setCounter((prev) => {
+        if (prev - interval >= 0) {
+          return prev - interval;
+        } else {
+          return 0;
+        }
+      });
+    }
+  }
+  return <div className='container'>
+    <h1>Counter Apps</h1>
+    <p>counter: {counter}</p>
+    <div className='inner'>
+      <input type="number" value={interval} onChange={(event) => handleCounterChange(event)}></input>
+      <p>Select Interval</p>
     </div>
-  );
-}
+    <button onClick={incrementCounter}>Increment</button>
+    <button onClick={decrementCounter}>Decrement</button>
+  </div>
+};
 
 export default App;
